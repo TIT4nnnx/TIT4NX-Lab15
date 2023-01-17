d5403cb649b082926f8280ab35a5dff38f8ca17c
Your Name <you@example.com>
2023-01-17 15:40:30 +0700

    TIT4NX Lab15_1

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+    T key;
+    int i, j;
+    for (i = 1; i < N; i++){
+        key = d[i];
+        j = i - 1;
+        while (j >= 0 && d[j] < key)
+        {
+            d[j + 1] = d[j];
+            j = j - 1;
+        }
+        d[j + 1] = key;
+        cout << "Pass "<< i <<":";
+        for(int k = 0; k < N;k++){
+            cout << d[k] << " ";
+        }
+        cout << "\n";
+    }
 }
 
 int main(){
@@ -17,3 +33,4 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
